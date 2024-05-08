2eba46779cdff46ecf0bff81fb3714d006302988
AbnerSLima <abnerdslima@gmail.com>
2024-05-08 18:28:29 -0300

    update

--- JS/script.js
+++ JS/script.js
@@ -4,30 +4,67 @@ const digimonImage = document.getElementById("digimon_image");
 
 const form = document.getElementById("form");
 const input = document.getElementById("input_search");
+const button1 = document.getElementById("btn-1");
+const button2 = document.getElementById("btn-2");
+const button3 = document.getElementById("btn-3");
+
+let digimonInitial = 1;
 
 const fetchDigimon = async (digimon) => {
   const url = await fetch(`https://digi-api.com/api/v1/digimon/${digimon}`);
-  const returne = await url.json();
-  return returne;
+  
+  if (url.status === 200) {
+    const returne = await url.json();
+    return returne;
+  }
 }
 
 const renderDigimon = async (digimon) => {
 
+  digimonName.innerHTML = 'Carregando...';
+  digimonNumber.innerHTML = '';
+
   const data = await fetchDigimon(digimon);
 
+  if(data) {
     digimonName.innerHTML = data.name;
     digimonNumber.innerHTML = data.id;
     digimonImage.src = data.images[0].href;
+    input.value = '';
+    digimonInitial = data.id;
+  } else {
+    digimonImage.src = './images/digimon-not-found.png';
+    digimonName.innerHTML = 'Não encontrado';
+    digimonNumber.innerHTML = '';
+  }
 }
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-
   renderDigimon(input.value)
-  input.value = '';
 })
 
+ button1.addEventListener('click', () => {
+  if (input.value === '') {
+    digimonInitial = 9999
+    renderDigimon(digimonInitial);
+  } else {
+    renderDigimon(input.value)
+  }
+ })
 
+button2.addEventListener('click', () => {
+  digimonInitial += 1;
+  renderDigimon(digimonInitial);
+})
 
+button3.addEventListener('click', () => {
+  if (digimonInitial > 1){
+    digimonInitial -= 1;
+    renderDigimon(digimonInitial);
+  }
+})
+
+renderDigimon(digimonInitial);
 
 
